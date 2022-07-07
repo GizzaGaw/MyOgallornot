5a76ed68482043ee606f3d439bf0de698b72644b
FlutLab.io <>
2022-07-07 08:30:15 +0000

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/ogalornot/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.ogalornot
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,212 @@
+import 'package:flutter/material.dart';
+import 'item.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Ogalornot',
+      theme: ThemeData(
+        // This is the theme of your application.
+        primarySwatch: Colors.blueGrey,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('Ogallornot'),
+          bottom: TabBar(
+            tabs: <Widget>[
+              Tab(
+                icon: Icon(Icons.grid_on),
+              ),
+              Tab(
+                icon: Icon(Icons.list),
+              ),
+            ],
+          ),
+        ),
+        body: TabBarView(
+          children: <Widget>[
+            // GridView tab content Widget
+            GridView.count(
+              // Items in row
+              crossAxisCount: 2,
+              // Vertical spacing between rows
+              mainAxisSpacing: 5.0,
+              // Horizontal spacing between columns
+              crossAxisSpacing: 5.0,
+              // Padding of GridView
+              padding: const EdgeInsets.all(5.0),
+              // The ratio between the width and height of items
+              childAspectRatio: 0.75,
+              // List of items widgets
+              children: items.map<Widget>((Item item) => _ItemGridCellWidget(item)).toList(),
+            ),
+            // ListView tab content Widget
+            ListView.builder(itemCount: items.length, itemBuilder: (BuildContext context, int position) => _ItemListCellWidget(items[position]))
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _ItemGridCellWidget extends StatelessWidget {
+  final Item _item;
+
+  _ItemGridCellWidget(this._item);
+
+  void _selectItem(BuildContext context) {
+    Navigator.push(
+        context,
+        MaterialPageRoute<void>(
+          builder: (BuildContext context) => _ItemFullScreenWidget(_item),
+        ));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GridTile(
+        footer: GridTileBar(
+          title: Text(_item.name),
+          subtitle: Text(_item.description),
+          backgroundColor: Colors.black38,
+        ),
+        child: GestureDetector(
+          onTap: () => _selectItem(context),
+          child: Hero(
+            key: Key(_item.imageUrl),
+            tag: _item.name,
+            child: Image.network(
+              _item.imageUrl,
+              fit: BoxFit.cover,
+            ),
+          ),
+        ));
+  }
+}
+
+class _ItemListCellWidget extends StatelessWidget {
+  final Item _item;
+
+  _ItemListCellWidget(this._item);
+
+  void _selectItem(BuildContext context) {
+    Navigator.push(
+        context,
+        MaterialPageRoute<void>(
+          builder: (BuildContext context) => _ItemFullScreenWidget(_item),
+        ));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      onTap: () => _selectItem(context),
+//      isThreeLine: true,
+      title: Text(
+        _item.name,
+        style: TextStyle(
+          fontWeight: FontWeight.w500,
+          fontSize: 20,
+        ),
+      ),
+      subtitle: Text(
+        _item.description,
+        maxLines: 2,
+        style: TextStyle(),
+      ),
+      leading: Hero(
+        key: Key(_item.imageUrl),
+        tag: _item.name,
+        child: Image.network(
+          _item.imageUrl,
+          width: 50,
+          height: 50,
+          fit: BoxFit.cover,
+        ),
+      ),
+    );
+  }
+}
+
+class _ItemFullScreenWidget extends StatelessWidget {
+  final Item _item;
+
+  _ItemFullScreenWidget(this._item);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(_item.name),
+      ),
+      body: SizedBox.expand(
+        child: Hero(
+          tag: _item.name,
+          child: Image.network(
+            _item.imageUrl,
+            fit: BoxFit.cover,
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+List<Item> items = [
+  Item(
+    "https://images.alphacoders.com/690/690854.jpg",
+    "Pict 1",
+    """Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+    consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+    proident, sunt in culpa qui officia deserunt mollit anim id est laborum.""",
+  ),
+  Item(
+    "https://images2.alphacoders.com/723/723572.jpg",
+    "Pict 2",
+    "Description",
+  ),
+  Item(
+    "https://images2.alphacoders.com/499/499590.jpg",
+    "Pict 3",
+    "Description",
+  ),
+  Item(
+    "https://images3.alphacoders.com/636/636472.jpg",
+    "Totoro",
+    "Description",
+  ),
+  Item(
+    "https://i.ytimg.com/vi/CI2gyevDC6Q/maxresdefault.jpg",
+    "Pict 5",
+    "Description",
+  ),
+  Item(
+    "https://i0.wp.com/jabarekspres.com/wp-content/uploads/2022/05/Ilustrasi-Anime-Spy-X-Family-Episode-6-e1652338420882.png?fit=1723%2C835&ssl=1",
+    "spy x fam",
+    "Description",
+  ),
+];
